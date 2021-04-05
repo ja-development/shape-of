a1c7b401715fe16124cc6b8895723361dfeb2d83
ja-development <38837123+ja-development@users.noreply.github.com>
2021-04-04 21:27:58 -0400

    updated readme and source versions

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# shapeOf v0.0.6
+# shapeOf v0.0.7
 A lightweight schema validator and object mutator for JSON endpoints and Plain Old JavaScript Objects (POJOs).
 
 Features include:
--- index.js
+++ index.js
@@ -1669,11 +1669,12 @@ _coreValidators.forEach(validator => {
 
 // NOTE: The following variables are automatically updated when running 'npm run-script update-versions'
 shapeOf._versionCompatibilityHistory = { // version: compatibleSchemaVersion
+	'0.0.7': '0.0.7',
 	'0.0.6': '0.0.6',
 	'0.0.5': '0.0.5',
 }; // end of shapeOf._versionCompatibilityHistory
-shapeOf.version = "0.0.6"; // core version
-shapeOf.compatibleSchemaVersion = "0.0.6"; // compatible schema version
+shapeOf.version = "0.0.7"; // core version
+shapeOf.compatibleSchemaVersion = "0.0.7"; // compatible schema version
 
 
 
